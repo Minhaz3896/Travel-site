b45439cc189b84503c32165afdbf33c48a120000
Minhaz <minhaz3896@gmail.com>
2023-11-14 23:52:12 +0600

    Navbar:Added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from "react";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
+       <Navbar/>
+       HELLO
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+// import React, {useState} from 'react'
+import {AiOutlineMenu} from 'react-icons/ai'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div>
+             <AiOutlineMenu/>
+        </div>
+        <h1>
+            Best <span> Eats</span>
+        </h1>
+        <div>
+            <p></p>
+            <p></p>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
