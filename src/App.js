c4442b4daad8689b985a1c15c5599cc3934001c7
Minhaz <minhaz3896@gmail.com>
2023-11-17 04:00:20 +0600

    Cards:Added Cards Catagory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Cards from "./Components/Cards";
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <Hero/>
+       <Cards/>
     </div>
   );
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='max-w-[1640px] mx-auto p-4'>
+      <div className='max-h-[500px] relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text 7xl font-bold text-orange-600'>The <span className='text-white'>Best</span> </h1>
+          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text 7xl font-bold text-white'>Foods <span className='text-orange-600'>Given</span></h1>
+        </div>
+        <img  className='w-full max-h-[500px] object-cover' src='https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/>
+      </div>
+
+    </div>
   )
 }
 
