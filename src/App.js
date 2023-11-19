091aa87f2bccbc91fe7a6791cf70ce595eae1011
Minhaz <minhaz3896@gmail.com>
2023-11-19 19:51:17 +0600

    Food: Food Component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Cards from "./Components/Cards";
+import Food from "./Components/Food";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
        <Navbar/>
        <Hero/>
        <Cards/>
+       <Food/>
     </div>
   );
 }
