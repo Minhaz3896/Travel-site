184a7a19e45ae97fd173728e8ba972677e420ac8
Minhaz <minhaz3896@gmail.com>
2023-11-22 17:31:46 +0600

    Foods:Added price and food filter functions

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Cards = () => {
   return (
     <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
@@ -7,8 +8,8 @@ const Cards = () => {
         <div className='rounded-xl relative' >
             {/* overlay */}
             <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                <p className='font-bold text-2xl px-2 pt-4'>BOGO's Out</p>
-                <p className='px-2'>Through 23/5</p>
+                <p className='font-bold text-2xl px-2 pt-4'>Burgers'n Fries</p>
+                <p className='px-2'>Offers 50% off</p>
                 <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/>
@@ -16,8 +17,8 @@ const Cards = () => {
         <div className='rounded-xl relative' >
             {/* overlay */}
             <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                <p className='font-bold text-2xl px-2 pt-4'>BOGO's Out</p>
-                <p className='px-2'>Through 23/5</p>
+                <p className='font-bold text-2xl px-2 pt-4'>Burgers'n Fries</p>
+                <p className='px-2'>Offers 50% off</p>
                 <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/>
@@ -25,8 +26,8 @@ const Cards = () => {
         <div className='rounded-xl relative' >
             {/* overlay */}
             <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                <p className='font-bold text-2xl px-2 pt-4'>BOGO's Out</p>
-                <p className='px-2'>Through 23/5</p>
+                <p className='font-bold text-2xl px-2 pt-4'>Burgers'n Fries</p>
+                <p className='px-2'>Offers 50% off</p>
                 <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/>
--- src/Components/Food.js
+++ src/Components/Food.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import{data} from '../Data/data.js'
 
-const Food = () => {
+
+
+const Food = () => {  
+    console.log(data); 
+    const [foods, setFoods] = useState(data) 
+
+    // Filter Food types
+    const filterType =(catagory) => {
+        setFoods(
+            data.filter((item) => {
+                return item.category === catagory;
+            })
+        )
+    }
+
+    // Filter Prices
+    const filterprice =(price) => {
+        setFoods(
+            data.filter((item) => {
+                return item.price === price;
+            })
+        )
+    }
+
   return (
+    
     <div className='max-w-[1640px] m-auto px-4 py-12'>
         <h1 className='text-orange-600 font-bold text-center text-4xl'>Our Menu</h1>
         {/* Filter */}
         <div className=' flex flex-col lg:flex-row justify-between'>
             {/* Type */}
+            
             <div>
                 <p className='font-bold text-gray-700'>Filter Type</p>
                 <div className='flex justify-between flex-wrap'>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burger</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Indian</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chinese</button>
+                    <button onClick={() => setFoods(data)} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
+                    <button onClick={() => filterType('burger')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burger</button>
+                    <button onClick={() => filterType('pizza')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
+                    <button onClick={() => filterType('indian')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Indian</button>
+                    <button onClick={() => filterType('chicken')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
                 </div>
             </div>
             
@@ -23,18 +49,33 @@ const Food = () => {
             <div>
                 <p className='font-bold text-gray-700'>Filter Price</p>
                 <div className='flex justify-between max-w-[390px] w-full'>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
-                    <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
+                    <button onClick={() => filterprice("$")} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white '>$</button>
+                    <button onClick={() => filterprice("$$")} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
+                    <button onClick={() => filterprice("$$$")} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
+                    <button onClick={() => filterprice("$$$$")} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
                 </div>
             </div>
         </div>
+        
 
         {/* All Foods */}
+        <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+            {foods.map ((item,index) => (
+                <div key={index} className='border shadow-lg hover:scale-95 duration-300'>
+                    <img src={item.image} alt={item.name}
+                    className='w-full h-[200px] object-cover rounded-t-lg'/>
+                    <div className='flex justify-between px-2 py-4'>
+                        <p className='font-bold'>{item.name}</p>
+                        <p>
+                            <span className='bg-yellow-500 text-white p-1 rounded'>{item.price}</span>
+                        </p>
+                    </div>
+                </div>
+                
+                
 
-        <div>
-
+            )) }
+        
         </div>
     </div>
   )
